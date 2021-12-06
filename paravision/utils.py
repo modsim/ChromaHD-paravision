36166b3b5b3412d4cd60185cd50549d871a7ee89
Jayghosh Rao <j.rao@fz-juelich.de>
2021-12-06 16:26:44 +0100

    add volume_integral.py

--- paravision/utils.py
+++ paravision/utils.py
@@ -172,6 +172,8 @@ def parse_cmdline_args():
     ap.add_argument("--bead-loading", action='store_true', help="Screenshot the given object")
     ap.add_argument("--radial-shell-integrate", help="Divide object radially and integrate")
     ap.add_argument("--column-snapshot", action='store_true', help="Snapshot the column with a semi-transparent container")
+    ap.add_argument("--volume-integral", action='store_true', help="Calculate AVERAGES using the volume integral. EXPERIMENTAL. TO BE REPLACED")
+
     ap.add_argument("-np", "--nproc", type=int, default=1, help="Screenshot the given object")
 
     ap.add_argument("--integrate", choices=['Volume', 'Area', 'None'], help="Integrate and average the given Volume/Area")
---
+++ paravision/volume_integral.py
@@ -0,0 +1,56 @@
+from paraview.simple import *
+from vtkmodules.numpy_interface import dataset_adapter as dsa
+import vtk.util.numpy_support as ns #type:ignore
+
+from paravision.utils import parse_cmdline_args, read_files, csvWriter
+
+# TODO: Update this with --integrate?
+
+def volume_integral(reader, args):
+    scalars = args['scalars'] or reader.PointArrayStatus
+
+    timeKeeper = GetTimeKeeper()
+    nts = len(reader.TimestepValues)
+
+    view = GetActiveViewOrCreate('RenderView')
+
+    # cellSize1 = CellSize(Input=reader)
+    # cellSize1.ComputeVolume= 1
+    # cellSize1.ComputeSum = 1
+    # volume = servermanager.Fetch(cellSize1)
+    # volume = dsa.WrapDataObject(volume)
+    # volume = volume.FieldData['Volume'][0]
+    # print("volume:", volume)
+
+    intOutput = {}
+    for scalar in scalars:
+        intOutput.update({scalar: []})
+        print(type(intOutput[scalar]))
+
+    for timestep in range(nts):
+
+        timeKeeper.Time = timestep
+        # print("Processing timestep: ", timestep, end="\r")
+        reader.UpdatePipeline(reader.TimestepValues[timestep])
+
+        integrated = IntegrateVariables(Input=reader)
+        intdata = servermanager.Fetch(integrated)
+        intdata = dsa.WrapDataObject(intdata)
+
+        volume = intdata.CellData['Volume'][0]
+
+        for scalar in scalars:
+            value = intdata.PointData[scalar]
+            value = ns.vtk_to_numpy(value)[0]/volume
+            intOutput[scalar].append(value)
+
+        Delete(integrated)
+
+    print(intOutput)
+    for scalar in scalars:
+        csvWriter(scalar + '.integrated.csv', reader.TimestepValues, intOutput[scalar])
+
+if __name__=="__main__":
+    args = parse_cmdline_args()
+    reader = read_files(args)
+    volume_integral(reader, args)
