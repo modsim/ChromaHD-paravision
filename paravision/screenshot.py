8b3eb185c09220f8e50c22fe26fd02dba6490b37
Jayghosh Rao <j.rao@fz-juelich.de>
2022-02-10 23:07:02 +0100

    screenshot: add colorbar customizations

--- paravision/screenshot.py
+++ paravision/screenshot.py
@@ -8,12 +8,13 @@ def screenshot(object, args, suffix=''):
     for key in args:
         print(key + ': ', args[key])
 
-    # args.scalars = args.scalars or reader.PointArrayStatus
-    args.scalars = args.scalars 
+    args.scalars = args.scalars or object.PointArrayStatus
+    # args.scalars = args.scalars 
     # scalars = args['scalars']
     # timeArray = reader.TimestepValues
 
     view = GetActiveViewOrCreate('RenderView')
+    view.OrientationAxesVisibility = args['show_axis']
 
     projection = project(object, args)
 
@@ -22,31 +23,60 @@ def screenshot(object, args, suffix=''):
 
         display = Show(projection, view)
         display.Representation = args['display_representation']
-        view.OrientationAxesVisibility = args['show_axis']
-        display.RescaleTransferFunctionToDataRange()
-        display.UpdatePipeline()
+        display.RescaleTransferFunctionToDataRange(False, True)
+        # RescaleTransferFunction(0.0, 0.007428335025126975)
 
         view_handler(args['view'], args['zoom'])
-        UpdateScalarBars()
+        # view.Update()
 
         if scalar == 'None':
             ColorBy(display, None)
         else:
             ColorBy(display, ('POINTS', scalar))
 
-
         wLUT = GetColorTransferFunction(scalar)
         wPWF = GetOpacityTransferFunction(scalar)
-        HideScalarBarIfNotNeeded(wLUT, view)
+        # HideScalarBarIfNotNeeded(wLUT, view)
 
-        wLUT.ApplyPreset('Rainbow Uniform', True)
+        wLUT.ApplyPreset(args['colormap'], True)
 
-        view.Update()
+        configure_scalar_bar(wLUT, view, scalar)
+
         UpdateScalarBars()
-
         display.SetScalarBarVisibility(view, args['show_scalar_bar'])
+        # view.Update()
+        # display.UpdatePipeline()
+
         SaveScreenshot('screenshot_' + scalar + '_' + suffix + '.png', view, ImageResolution=args['geometry'], TransparentBackground=1)
         Hide(display, view)
+
+def configure_scalar_bar(wLUT, view, title, fontfamily='Times', fontsize=18 ):
+    ColorBar = GetScalarBar(wLUT, view)
+    ColorBar.Orientation = 'Horizontal'
+    ColorBar.WindowLocation = 'LowerCenter'
+    ColorBar.Title = title
+    ColorBar.ComponentTitle = ''
+    ColorBar.TitleJustification = 'Centered'
+    ColorBar.HorizontalTitle = 1
+    ColorBar.TitleColor = [0.0, 0.0, 0.0]
+    ColorBar.TitleOpacity = 1.0
+    ColorBar.TitleFontFamily = fontfamily
+    ColorBar.TitleBold = 0
+    ColorBar.TitleItalic = 0
+    ColorBar.TitleShadow = 0
+    ColorBar.TitleFontSize = fontsize
+    ColorBar.LabelColor = [0.0, 0.0, 0.0]
+    ColorBar.LabelOpacity = 1.0
+    ColorBar.LabelFontFamily = fontfamily
+    ColorBar.LabelFontFile = ''
+    ColorBar.LabelBold = 0
+    ColorBar.LabelItalic = 0
+    ColorBar.LabelShadow = 0
+    ColorBar.LabelFontSize = fontsize
+    ColorBar.AutomaticLabelFormat = 1
+    ColorBar.LabelFormat = '%-#6.3g'
+    ColorBar.ScalarBarThickness = 20
+    ColorBar.ScalarBarLength = 0.50
 
 if __name__=="__main__":
     args = parse_cmdline_args()
