b5ceb69ef0ac267eb637fbe374e86bfea0bd8769
Jayghosh Rao <j.rao@fz-juelich.de>
2022-03-20 13:32:31 +0100

    re-add nproc in cmdline args

--- paravision/utils.py
+++ paravision/utils.py
@@ -205,6 +205,8 @@ def parse_cmdline_args():
     ap.add_argument("--animate", action='store_true', help="Create animation as series of pngs")
     ap.add_argument("--infogeneric", action='store_true', help="Dump mesh info for generic field meshes")
 
+    ap.add_argument("-np", "--nproc", type=int, default=1, help="Screenshot the given object")
+
     ap.add_argument("--integrate", choices=['Volume', 'Area', 'None'], help="Integrate and average the given Volume/Area")
     # ap.add_argument("--project", nargs=4, default=['clip', 'Plane', 0.5, "x"], help="Projection. <clip|slice> <Plane|Cylinder..> <origin> <x|y|z>" )
     ap.add_argument("--project", nargs=4, default=['none', 'Plane', 0.5, "x"], help="Projection. <clip|slice|none> <Plane|Cylinder..> <origin> <x|y|z>" )
