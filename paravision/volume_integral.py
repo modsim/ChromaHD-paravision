ba52bd3b6fe030f4c3e8c155f9cee8862f95f069
Jayghosh Rao <j.rao@fz-juelich.de>
2022-03-31 00:03:25 +0200

    updated volume_integral

--- paravision/volume_integral.py
+++ paravision/volume_integral.py
@@ -1,30 +1,58 @@
 from paraview.simple import *
-from vtkmodules.numpy_interface import dataset_adapter as dsa
-import vtk.util.numpy_support as ns #type:ignore
 
-from paravision.utils import parse_cmdline_args, read_files, csvWriter
+from paravision.utils import read_files, csvWriter
 from paravision.integrate import integrate
 
 # TODO: Update this with --integrate?
 
+from paravision import ConfigHandler
+import argparse
+from addict import Dict
+
+from rich import print, print_json
+
 def volume_integral(reader, args):
-    for key in args:
-        print(key + ': ', args[key])
+    """ Calculate the integral of a scalar over a volume
+    """
     scalars = args['scalars'] or reader.PointArrayStatus
 
-    timeKeeper = GetTimeKeeper()
-    nts = len(reader.TimestepValues)
+    # timeKeeper = GetTimeKeeper()
+    # nts = len(reader.TimestepValues)
     timeArray = reader.TimestepValues
 
     view = GetActiveViewOrCreate('RenderView')
 
-    result = integrate(reader, scalars, normalize=args['volume_integral'], timeArray=timeArray)
+    result = integrate(reader, scalars, normalize=args.normalize, timeArray=timeArray)
 
     print(result)
     for i,scalar in enumerate(scalars):
-        csvWriter(scalar + '.integrated.csv', reader.TimestepValues, map(lambda x: x[i], result))
+        filename = scalar + '.integrated.csv'
+        print(f'Writing data to {filename}')
+        csvWriter(filename, reader.TimestepValues, map(lambda x: x[i], result))
+
+
+def volume_integral_parser(args, local_args_list):
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-n", "--normalize", choices = ['NoNorm', 'Volume', 'Area'], help="files..")
+    ap.add_argument("FILES", nargs='*', help="files..")
+
+    print(local_args_list)
+
+    local_args = ap.parse_args(local_args_list)
+    local_args = Dict(vars(local_args))
+
+    print_json(data=local_args)
+
+    args.update([ (k,v) for k,v in local_args.items() if v is not None])
+
+    return args
+
 
 if __name__=="__main__":
-    args = parse_cmdline_args()
+    config = ConfigHandler()
+    args, local_args_list = config.parse_config_and_cmdline_args()
+    args = volume_integral_parser(args, local_args_list)
+
     reader = read_files(args['FILES'], filetype=args['filetype'])
     volume_integral(reader, args)
