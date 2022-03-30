65ce2aeab601670b2902e8ee112c1484c092deae
Jayghosh Rao <j.rao@fz-juelich.de>
2022-03-30 23:46:35 +0200

    update column_snapshot

--- paravision/column_snapshot.py
+++ paravision/column_snapshot.py
@@ -1,25 +1,22 @@
 from paraview.simple import *
+from vtk import vtkThreshold
 
-from paravision.utils import parse_cmdline_args, read_files, view_handler
+from paravision.utils import read_files, view_handler
 from paravision.project import project
 
+from paravision import ConfigHandler
+import argparse
+from addict import Dict
+
+from rich import print, print_json
+
 def column_snapshot(reader, args):
-
-    for key in args:
-        print(key + ': ', args[key])
-
-    geometry    = args['geometry']
-    axisVisible = args['show_axis']
-    zoom        = args['zoom']
-    files       = args['FILES']
-    filetype    = args['filetype']
 
     view = GetActiveViewOrCreate('RenderView')
     connectivity = Connectivity(Input=reader)
     connectivityDisplay = Show(connectivity, view)
 
-    connectivityDisplay.Representation = args['display_representation']
-    view.OrientationAxesVisibility = int(axisVisible)
+    view.OrientationAxesVisibility = int(args.show_axis)
 
     nord10 = [94, 129, 172]
     color_rgb = [ x/255 for x in nord10]
@@ -37,21 +34,32 @@ def column_snapshot(reader, args):
     # print("Processing bead: {index}".format(index=index))
     print("Processing beads: {nbeads}".format(nbeads=nbeads))
     threshold = Threshold(Input=connectivity)
-    threshold.ThresholdRange = [0, nbeads-1]
+    # threshold.ThresholdRange = [0, nbeads-1]
+    threshold.LowerThreshold = 0
+    threshold.UpperThreshold = nbeads-1
+    threshold.ThresholdMethod = vtkThreshold.THRESHOLD_BETWEEN
     thresholdDisplay = Show(threshold, view)
     ColorBy(thresholdDisplay, None)
     # threshold.UpdatePipeline()
     thresholdDisplay.AmbientColor = color_rgb
     thresholdDisplay.DiffuseColor = color_rgb
+    thresholdDisplay.Representation = args.display_representation
 
     # thresholdDisplay.AmbientColor = [2/255, 61/255, 107/255]
     # thresholdDisplay.DiffuseColor = [2/255, 61/255, 107/255]
 
     print("Processing Column.")
     threshold = Threshold(Input=connectivity)
-    threshold.ThresholdRange = [nbeads, nbeads]
-    # outerShell = Projection(threshold, 'clip')
-    outerShell = project(threshold, args)
+    # threshold.ThresholdRange = [nbeads, nbeads]
+    threshold.LowerThreshold = nbeads
+    threshold.UpperThreshold = nbeads
+    threshold.ThresholdMethod = vtkThreshold.THRESHOLD_BETWEEN
+
+    outerShell = project(
+            threshold, 
+            {
+                'project': ['clip', 'Plane', 0.5, 'x']
+            })
 
     outerShellDisplay = Show(outerShell, view)
     ColorBy(outerShellDisplay, None)
@@ -63,12 +71,37 @@ def column_snapshot(reader, args):
 
     view.Update()
     view.ResetCamera()
-    view.ViewSize = geometry
+    view.ViewSize = args.geometry
 
     ## NOTE: Only works on the first file provided
-    SaveScreenshot('column_snapshot.png', view, ImageResolution=geometry, TransparentBackground=1)
+    screenshot_filename = f'column_snapshot.png'
+    print(f'Saving screenshot to file: {screenshot_filename}')
+    SaveScreenshot(screenshot_filename, view, ImageResolution=args.geometry, TransparentBackground=1)
+
+def column_snapshot_parser(args, local_args_list):
+
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument("FILES", nargs='*', help="files..")
+
+    print(local_args_list)
+
+    local_args = ap.parse_args(local_args_list)
+    local_args = Dict(vars(local_args))
+
+    print_json(data=local_args)
+
+    args.update([ (k,v) for k,v in local_args.items() if v is not None])
+
+    return args
 
 if __name__=="__main__":
-    args = parse_cmdline_args()
+    config = ConfigHandler()
+    args, local_args_list = config.parse_config_and_cmdline_args()
+    args = column_snapshot_parser(args, local_args_list)
+
+    print("[bold yellow]Final set of args:[/bold yellow]")
+    print_json(data=args)
+
     reader = read_files(args['FILES'], filetype=args['filetype'])
     column_snapshot(reader, args)
