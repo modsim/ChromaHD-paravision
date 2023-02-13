05c8a92a685aa2efe3bb124ab05f1d4fb0e73bd6
Jayghosh Rao <j.rao@fz-juelich.de>
2023-02-13 22:46:38 +0100

    overhaul hotspots

--- paravision/hotspots.py
+++ paravision/hotspots.py
@@ -25,38 +25,57 @@ from addict import Dict
 
 from rich import print, print_json
 
-def hotspots(object, args):
+from paravision.defaults import DEFAULT_CONFIG
+
+def hotspots(object, **args):
     """ Screenshot a given object with a given projection"""
 
-    args.scalars = args.scalars or object.PointArrayStatus
+    _scalars               = args.get('scalars') or object.PointArrayStatus
+    _threshold_reference   = args.get('threshold_reference'   , DEFAULT_CONFIG.threshold_reference) 
+    _threshold_factor   = args.get('threshold_factor'   , DEFAULT_CONFIG.threshold_factor) 
+
+    _project               = args.get('project'               , DEFAULT_CONFIG.project) 
+    _show_axis             = args.get('show_axis'             , DEFAULT_CONFIG.show_axis)
+    _geometry              = args.get('geometry'              , DEFAULT_CONFIG.geometry)
+    _view                  = args.get('view'                  , DEFAULT_CONFIG.view)
+    _zoom                  = args.get('zoom'                  , DEFAULT_CONFIG.zoom)
+    _show_scalar_bar       = args.get('show_scalar_bar'       , DEFAULT_CONFIG.show_scalar_bar)
+    _display_representation= args.get('display_representation', DEFAULT_CONFIG.display_representation)
+    _output_prefix         = args.get('output_prefix'         , DEFAULT_CONFIG.output_prefix)
+
+    # These aren't directly used here, but passed as args in handle_coloring
+    _colormap              = args.get('colormap'              , DEFAULT_CONFIG.colormap)
+    _colormap_fuzzy_cutoff = args.get('colormap_fuzzy_cutoff' , DEFAULT_CONFIG.colormap_fuzzy_cutoff)
+    _color_range_method    = args.get('color_range_method'    , DEFAULT_CONFIG.color_range_method)
+    _custom_color_range    = args.get('custom_color_range'    , DEFAULT_CONFIG.custom_color_range)
 
     view = GetActiveViewOrCreate('RenderView')
-    view.OrientationAxesVisibility = args['show_axis']
-    view.ViewSize = args['geometry']
+    view.OrientationAxesVisibility = _show_axis
+    view.ViewSize = _geometry
 
-    projection = projector(object, *args.project)
+    projection = projector(object, *_project)
 
     display = Show(projection, view)
-    view_handler(args['view'], args['zoom'])
+    view_handler(_view, _zoom)
     Hide(projection, view)
 
-    for scalar in args['scalars']:
+    for scalar in _scalars:
         print("Snapping", scalar )
 
-        threshold = create_threshold(projection, scalar, 'above', 0, args.threshold_reference * args.threshold_factor)
+        threshold = create_threshold(projection, scalar, 'above', 0, _threshold_reference * _threshold_factor)
 
         display = Show(threshold, view)
-        display.Representation = args.display_representation
+        display.Representation = _display_representation
 
         wLUT, _ = handle_coloring(threshold, display, scalar, args)
 
         configure_scalar_bar(wLUT, view, config.get('ColorBar'))
         UpdateScalarBars()
-        display.SetScalarBarVisibility(view, args['show_scalar_bar'])
+        display.SetScalarBarVisibility(view, _show_scalar_bar)
 
-        screenshot_filename = f'screenshot_{args.output_prefix}_{scalar}.png'
+        screenshot_filename = f'hotspot_{_threshold_factor}x{_threshold_reference}_{_output_prefix}_{scalar}.png'
         print(f'Saving screenshot to file: {screenshot_filename}')
-        SaveScreenshot(screenshot_filename, view, ImageResolution=args['geometry'], TransparentBackground=1)
+        SaveScreenshot(screenshot_filename, view, ImageResolution=_geometry, TransparentBackground=1)
 
         Delete(display)
         Delete(threshold)
@@ -94,7 +113,7 @@ if __name__=="__main__":
 
         if args['append_datasets']:
             appended = AppendDatasets(Input=readers)
-            hotspots(appended, args)
+            hotspots(appended, **args)
         else: 
             print("ERROR: Screenshotting for pure --standalone not yet fully supported. Please use along with --append-datasets")
 
@@ -105,5 +124,5 @@ if __name__=="__main__":
             #     screenshot(ireader, args)
     else: 
         reader = read_files(args['FILES'], filetype=args['filetype'], standalone=args['standalone'])
-        hotspots(reader, args)
+        hotspots(reader, **args)
 
